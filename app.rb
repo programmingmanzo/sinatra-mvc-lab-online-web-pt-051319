399be13f39eecc1feba477604e638ca546bcfcbc
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-26 03:43:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  require_relative 'config/environment'
+
+class App < Sinatra::Base
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        @user_phrase = params[:user_phrase]
+        erb :piglatinize
+    end
 end
