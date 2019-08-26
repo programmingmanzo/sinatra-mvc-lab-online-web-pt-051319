cd03739650c6857ae8f5d03a86b4bdf0d75910cd
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-26 03:44:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,10 +1,6 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
-  require_relative 'config/environment'
-
-class App < Sinatra::Base
     get '/' do
         erb :user_input
     end
